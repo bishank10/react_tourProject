1b4a9fd05f186f0c17ec909d45a182acc1b7ff9f
bishank <chandbishank@gmail.com>
2020-11-27 13:23:48 -0700

    all the buttons in the page made functional

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [tours,setTours]=useState([]);
   const [loading,setLoading]=useState(true);
 
+  const removeTour = (id)=>{
+    const newTour = tours.filter((tour)=> tour.id !== id)
+    setTours(newTour);
+  }
+
   const fetchTours = async()=> {
     setLoading(true);;
   
@@ -35,7 +40,7 @@ function App() {
   }
 
   return (<main>
-    <Tours tours={tours} />
+    <Tours tours={tours}  removeTour={removeTour}/>
   </main>)
 }
 
